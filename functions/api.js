08f6c95850a038d8a297cc1a0d1309fbb586cc24
jota-ele-ene <hi@jln.bz>
2022-01-11 16:36:24 +0100

    Reverting functions

--- functions/api.js
+++ functions/api.js
@@ -1,16 +1,7 @@
 exports.handler = async (event, context) => {
-  console.log('process.env.MY_ENV_VAR', process.env.MY_ENV_VAR)
-  const response = {
+  console.log('process.env.MY_ENV_VAR', process.env.MY_ENV_VAR);
+  return {
     statusCode: 200,
-    body: JSON.stringify({
-      headers: {
-        /* Required for CORS support to work */
-        "Access-Control-Allow-Origin" : "*",
-        /* Required for cookies, authorization headers with HTTPS */
-        "Access-Control-Allow-Credentials" : true
-      },
-      message: `hello ${process.env.MY_ENV_VAR}`
-    }),
-  }
-  return callback(null, response)
+    body: JSON.stringify({message: event.queryStringParameters})
+  };
 }
