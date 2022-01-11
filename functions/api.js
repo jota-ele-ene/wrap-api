b4267d2fda3d4dbffe2064e6873e73266df14eeb
jota-ele-ene <hi@jln.bz>
2022-01-11 19:15:35 +0100

    Reverting functions

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,8 @@ exports.handler = async (event, context) => {
 
   const MY_URL_DEFAULT = "https://reqres.in/api/users";
   const MY_PARAM_DEFAULT = "page=2";
-  let myurl = `${process.env.MY_API_URL}?`;
+  //let myurl = `${process.env.MY_API_URL}?`;
+  let myurl = process.env.MY_API_URL;
   let myparam = process.env.MY_PARAM;
   console.log("Variables: "+myurl+","+myparam);
 
@@ -23,7 +24,9 @@ exports.handler = async (event, context) => {
   if (Boolean(myparam)==false) {
     myparam = MY_PARAM_DEFAULT;
   }
-
+  if (Boolean(event.rawQuery)) {
+    myparam += event.rawQuery;
+  }
   message = "URL set to " + myurl + "?" + myparam;
 
   return {
