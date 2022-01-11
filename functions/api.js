fb1aa991423ad7dca6ae1f1c4be51631022fee03
jota-ele-ene <hi@jln.bz>
2022-01-11 17:16:30 +0100

    Reverting functions

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   console.log('process.env.MY_ENV_VAR', process.env.MY_ENV_VAR);
-  const myurl = process.env.MY_API_URL;
-  const myparam = process.env.MY_PARAM;
+  let myurl = process.env.MY_API_URL;
+  let myparam = process.env.MY_PARAM;
   if (myurl)
     console.log('process.env.MY_API_URL set: ', myurl);
   else {
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
     console.log('process.env.MY_PARAM set: ', myparam);
   else {
     myurl = process.env.MY_PARAM_DEFAULT;
-    console.log('process.env.MY_PARAM unset. Reading DEFAULT ', myparam);    
+    console.log('process.env.MY_PARAM unset. Reading DEFAULT ', myparam);
   }
   return {
     statusCode: 200,
